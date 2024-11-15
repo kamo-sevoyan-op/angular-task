4e40d27d0a966f08d9edae97a573469c7f5a84a3
Kamo Sevoyan <kamo@Kamo-Sevoyan-MBP.local>
2024-11-15 19:34:32 +0400

    Add 'getDataById' 'updateUserData' 'getUsersData' functions to UsersService class.

--- src/app/users/users.service.ts
+++ src/app/users/users.service.ts
@@ -1,30 +1,62 @@
-import { Injectable } from "@angular/core";
-import { User } from "./user/user.model";
-import { filter } from "rxjs";
+import { Injectable, OnInit } from '@angular/core';
+import { User } from './user/user.model';
+import { filter } from 'rxjs';
 
 @Injectable({
-    providedIn: "root"
+  providedIn: 'root',
 })
 export class UsersService {
-    private data: ({id: string} & User)[] = [
-        {
-            id: "0",
-            email: "kamo.sevoyan.am@gmail.com",
-            dateOfBirth: "1998 06 13",
-            firstName: "Kamo",
-            middleName: "Koryun",
-            lastName: "Sevoyan",
-            isActivated: "activated",
-            profileImageUrl: "localhost:4000/img.png",
-            nationality: "armenian",
-            phoneNumber: "+37493047450",
-            gender: "male",
-            mainLanguage: "en",
-            recitations: "none"
-        }
-    ]
+  private data = new Map<string, User>();
 
-    getDataById(userId: string){
-        return this.data!.find((user) => user.id === userId);
-    }
+  constructor() {
+    this.data.set('0', {
+      email: 'kamo.sevoyan.am@gmail.com',
+      dateOfBirth: '1998 06 13',
+      firstName: 'Kamo',
+      middleName: 'Koryun',
+      lastName: 'Sevoyan',
+      status: 'Active',
+      profileImageUrl: 'localhost:4000/img.png',
+      nationality: 'armenian',
+      phoneNumber: '+37493047450',
+      gender: 'male',
+      mainLanguage: 'en',
+      recitations: 'none',
+    });
+
+    this.data.set('1', {
+      email: 'adam.eva.snake@heaven.jw',
+      dateOfBirth: '1967 1 1',
+      firstName: 'Adam',
+      middleName: 'Eva',
+      lastName: 'Snake',
+      status: 'Active',
+      profileImageUrl: 'localhost:4000/img.png',
+      nationality: 'armenian',
+      phoneNumber: '+37493047450',
+      gender: 'male',
+      mainLanguage: 'en',
+      recitations: 'none',
+    });
+  }
+
+  getDataById(userId: string) {
+    return this.data.get(userId);
+  }
+
+  updateUserData(userId: string, user: User) {
+    this.data.set(userId, user);
+  }
+
+  getUsersData() {
+    const userArray = Array.from(this.data, ([id, user]) => ({
+      id: id,
+      firstName: user.firstName,
+      lastName: user.lastName,
+      email: user.email,
+      status: user.status,
+    }));
+
+    return userArray;
+  }
 }
