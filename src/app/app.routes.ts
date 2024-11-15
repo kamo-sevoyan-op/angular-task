aafaeba37249d404385ef3e1ab032ea3302067ea
Kamo Sevoyan <kamo@Kamo-Sevoyan-MBP.local>
2024-11-15 16:26:15 +0400

    Add routes for users, user/:userId and fallback routers.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,24 @@
 import { Routes } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
+import { UsersListComponent } from './users/users.component';
+import { UserComponent } from './users/user/user.component';
+import { FallbackComponent } from './fallback/fallback.component';
 
 export const routes: Routes = [
     {
         path: "",
         component: HeaderComponent
     },
+    {
+        path: "users",
+        component: UsersListComponent
+    },
+    {
+        path: "user/:userId",
+        component: UserComponent
+    },
+    {
+        path: "**",
+        component: FallbackComponent
+    }
 ];
