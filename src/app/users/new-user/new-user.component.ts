0ce81ee75d6f690b21af2e32eb12919457c8a7a2
Kamo Sevoyan <kamo@Kamo-Sevoyan-MBP.local>
2024-11-16 00:07:01 +0400

    Add NewUser component.

---
+++ src/app/users/new-user/new-user.component.ts
@@ -0,0 +1,27 @@
+import { Component, inject } from '@angular/core';
+import { InputFormComponent } from '../../shared/input-form/input-form.component';
+import { User } from '../user/user.model';
+import { UsersService } from '../users.service';
+import { Router } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+
+@Component({
+  selector: 'app-new-user',
+  standalone: true,
+  imports: [InputFormComponent, MatButtonModule],
+  templateUrl: './new-user.component.html',
+  styleUrl: './new-user.component.css',
+})
+export class NewUserComponent {
+  userService = inject(UsersService);
+  router = inject(Router);
+
+  onCreateUser(event: User) {
+    this.userService.addUser(event);
+    this.router.navigate(['/users']);
+  }
+
+  onCancel() {
+    this.router.navigate(['./users']);
+  }
+}
