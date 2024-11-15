65e27ac34ae251079b1df153584b4e851b284f39
Kamo Sevoyan <kamo@Kamo-Sevoyan-MBP.local>
2024-11-15 19:38:24 +0400

    Update UsersComponent.

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,14 +1,32 @@
-import { Component, inject } from '@angular/core';
-import {MatTableModule} from '@angular/material/table';
+import { ChangeDetectorRef, Component, inject, input } from '@angular/core';
+import { MatTableModule } from '@angular/material/table';
 import { UsersService } from './users.service';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatPaginator, MatPaginatorIntl, MatPaginatorModule } from '@angular/material/paginator';
+import { ViewChild } from '@angular/core';
+import {MatMenuModule} from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon'
 
 @Component({
   selector: 'app-user-list',
   standalone: true,
-  imports: [MatTableModule],
+  imports: [MatTableModule, MatPaginatorModule, MatMenuModule, MatIconModule],
   templateUrl: './users.component.html',
-  styleUrl: './users.component.css'
+  styleUrl: './users.component.css',
 })
 export class UsersListComponent {
-  useListService = inject(UsersService)
+
+  usersService = inject(UsersService);
+  selectedColumns = ['id', 'firstName', 'lastName', 'email', 'status', "Action"];
+  dataSource = new MatTableDataSource(this.usersService.getUsersData());
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
+  constructor(){
+    this.paginator = new MatPaginator(new MatPaginatorIntl(), ChangeDetectorRef.prototype);
+  }
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+  }
 }
