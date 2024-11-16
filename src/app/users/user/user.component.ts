3e208331bf837a8250c28afaad8278b81a1613b6
Kamo Sevoyan <kamo@Kamo-Sevoyan-MBP.local>
2024-11-16 22:20:34 +0400

    Fix redirect to nonexisting user data bug.

--- src/app/users/user/user.component.ts
+++ src/app/users/user/user.component.ts
@@ -1,24 +1,26 @@
-import { Component, inject, input } from '@angular/core';
+import { Component, computed, inject, input } from '@angular/core';
 import { InputFormComponent } from '../../shared/input-form/input-form.component';
 import { UsersService } from '../users.service';
 import { Router } from '@angular/router';
 import { User } from './user.model';
+import { FallbackComponent } from '../../not-found/not-found.component';
 
 @Component({
   selector: 'app-user',
   standalone: true,
-  imports: [InputFormComponent],
+  imports: [InputFormComponent, FallbackComponent],
   templateUrl: './user.component.html',
   styleUrl: './user.component.css',
   providers: [],
 })
 export class UserComponent {
-  userId = input<string>();
+  userId = input.required<string>();
   usersService = inject(UsersService);
   router = inject(Router);
+  userExists = computed(() => this.usersService.contains(this.userId()));
 
   onUpdateUser(event: User) {
-    this.usersService.updateUserData(this.userId()!, event);
+    this.usersService.updateUserData(this.userId(), event);
     this.router.navigate(['/users']);
   }
 }
