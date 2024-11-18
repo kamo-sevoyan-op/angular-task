8e58c14377330f92b6f0638a3476d7d175732b68
Kamo Sevoyan <kamo@Kamo-Sevoyan-MBP.local>
2024-11-18 14:06:28 +0400

    Remove dummy data from UsersService.

--- src/app/users/users.service.ts
+++ src/app/users/users.service.ts
@@ -8,40 +8,6 @@ export class UsersService {
   private data = new Map<string, User>();
   private maxIndex: number = 1;
 
-  constructor() {
-    this.data.set('0', {
-      email: 'albert.einstein.ge@gmail.ge',
-      dateOfBirth: '1921 11 06',
-      firstName: 'Albert',
-      middleName: 'Einstein',
-      lastName: 'Einstein',
-      status: 'Active',
-      profileImageUrl:
-        'https://static.vecteezy.com/system/resources/previews/005/544/718/non_2x/profile-icon-design-free-vector.jpg',
-      nationality: 'german',
-      phoneNumber: '+123456789',
-      gender: 'male',
-      mainLanguage: 'ge',
-      recitations: 'none',
-    });
-
-    this.data.set('1', {
-      email: 'max.plank.ge@gmail.ge',
-      dateOfBirth: '1967 1 1',
-      firstName: 'Max',
-      middleName: 'Planck',
-      lastName: 'Planck',
-      status: 'Active',
-      profileImageUrl:
-        'https://static.vecteezy.com/system/resources/previews/005/544/718/non_2x/profile-icon-design-free-vector.jpg',
-      nationality: 'german',
-      phoneNumber: '+123456789',
-      gender: 'male',
-      mainLanguage: 'ge',
-      recitations: 'none',
-    });
-  }
-
   getDataById(userId: string) {
     return this.data.get(userId);
   }
